0e86b0376558bbd2d9161108f12340106b74fac2
Antoni Boucher <antoni.boucher@samsung.com>
2018-07-23 13:54:19 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// First crates to implement:
+// * rand
+// * logging
+// * cli argument parsing
+// * * getopts looks interesting.
+// * metrics (probably trivial-statsd)
+// * date/time
+// * * utc / timezone
+// * * monotone
+// * * format
+// * * tm struct
